5addfe39db5fa1c3205f572c30bf6373193c4e40
Lachlan MacKenzie <lachlanrm@gmail.com>
2016-12-09 13:47:39 -0800

    Defining MessagePage component

--- src/components/messages/MessagePage.js
+++ src/components/messages/MessagePage.js
@@ -1,1 +1,20 @@
-MessagePage.js
+import React, {PropTypes} from 'react';  
+import {connect} from 'react-redux';  
+import * as messageActions from '../../actions/messageActions';
+
+class MessagePage extends React.Component {  
+  render() {
+    return()
+  }
+}
+
+
+MessagePage.propTypes = {
+
+};
+
+function mapStateToProps(state, ownProps) {
+
+} 
+
+export default connect(mapStateToProps)(MessagePage); 
