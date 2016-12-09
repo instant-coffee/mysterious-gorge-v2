ea3891ef301bf3a3336b8fb85c72e5aee175aee9
Lachlan MacKenzie <lachlanrm@gmail.com>
2016-12-09 10:48:40 -0800

    Adding routes for components

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';  
+import { Route, IndexRoute } from 'react-router';  
+import App from './components/App';  
+import HomePage from './components/home/HomePage';  
+import MessagesPage from './components/messages/MessagesPage';  
+import MessagePage from './components/messages/MessagePage';
+
+export default (  
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="/messages" component={MessagesPage} />
+  </Route>
+);
