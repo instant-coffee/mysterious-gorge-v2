7ab9975ab4ca3fa17c1f6df0b1e549b007d69885
Lachlan MacKenzie <lachlanrm@gmail.com>
2016-12-09 13:12:54 -0800

    Adding InitialState and Message Reducers

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';  
+import messages from './messageReducer';
+
+const rootReducer = combineReducers({  
+  // short hand property names instead of messages: messages
+  messages
+})
+
+export default rootReducer;  
