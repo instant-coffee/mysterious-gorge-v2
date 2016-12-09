50b1ab11c25c21391f8fde5bb72ef78dddc6e6b3
Lachlan MacKenzie <lachlanrm@gmail.com>
2016-12-09 11:09:23 -0800

    Adding Navigation Header component

---
+++ src/components/common/Header.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from 'react';  
+import { Link, IndexLink } from 'react-router';
+
+const Header = () => {  
+  return (
+    <nav>
+      <IndexLink to="/" 
+        activeClassName="active">Home</IndexLink>
+      {" | "}
+      <Link to="/messages" activeClassName="active">Messages</Link>
+    </nav>
+  );
+};
+
+export default Header;
