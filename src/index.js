e1672975a3216053719c560774945897ced385cb
Lachlan MacKenzie <lachlanrm@gmail.com>
2016-12-09 11:53:32 -0800

    Passing the Store with Redux Provider

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import React from 'react';
 import { render } from 'react-dom';  
 import { Router, browserHistory } from 'react-router';  
 import routes from './routes';
+import configureStore from './store/configureStore';  
+
+const store = configureStore();
 
 render(  
- <Router history={browserHistory} routes={routes} />,
- document.getElementById('app')
+ 	<Provider store={store}>
+ 		<Router history={browserHistory} routes={routes} />
+ 	</Provider>,
+  document.getElementById('app')
 );
