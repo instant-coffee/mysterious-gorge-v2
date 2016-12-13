6dae7df3f8f681c4b5a0e97288a2dc3ed483114e
Lachlan MacKenzie <lachlanrm@gmail.com>
2016-12-12 19:56:01 -0800

    Adding DELETE method call

--- src/api/MysteriousGorgeApi.js
+++ src/api/MysteriousGorgeApi.js
@@ -1,6 +1,19 @@
 class MysteriousGorgeApi {  
   static getAllMessages() {
+    
     return fetch('https://mysterious-gorge-83229.herokuapp.com/messages/').then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
+
+  static deleteMessage(message) {
+    const request = new Request(`https://mysterious-gorge-83229.herokuapp.com/messages/${message.id}`, {
+      method: 'DELETE'
+    });
+
+    return fetch(request).then(response => {
       return response.json();
     }).catch(error => {
       return error;
